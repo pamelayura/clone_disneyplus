86dba178c5db8f7de9ffb354e65f833e9616a510
Pamela Yura <pamela_yura@hotmail.com>
2024-04-22 16:51:45 -0300

    criando seção de atrações

---
+++ src/main.js
@@ -0,0 +1,30 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const buttons = document.querySelectorAll('[data-tab-button]');
+
+    for (let i = 0; i < buttons.length; i++) {
+        buttons[i].addEventListener('click', function(botao) {
+            const abaAlvo = botao.target.dataset.tagButton;
+            const aba = document.querySelector(`[data-tab-id=${abaAlvo}]`);
+            escondeTodasAbas();
+            aba.classList.add('shows__list--is-active');
+            removeBotaoAtivo();
+            botao.target.classList.add('shows__tabs__button--is-active');
+        })
+    }
+})
+
+function removeBotaoAtivo() {
+    const buttons = document.querySelectorAll('[data-tab-button]');
+
+    for (let i = 0; i < buttons.length; i++) {
+        buttons[i].classList.remove('shows__tabs__button--is-active');
+    }
+}
+
+function escondeTodasAbas() {
+    const tabsContaineir = document.querySelectorAll('[data-tab-id]');
+
+    for (let i = 0; i < tabsContaineir.length; i++) {
+        tabsContaineir[i].classList.remove('shows__list--is-active');
+    }
+}
